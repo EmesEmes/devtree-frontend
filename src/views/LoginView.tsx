06dffff00b03945f406d6c99de6c9f407c69f271
Emilio del Hierro <delhierroemilio@gmail.com>
2025-05-10 23:23:44 -0500

    Layout de autenticacioooo2n creado, implementado react-hook-form y axios, componente de errores creado, agregado validaciones, registro de usuarios creado

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router"
 const LoginView = () => {
   return (
     <>
-      <div>Login View</div>
-      <nav>
-        <Link to={"/auth/register"}>
-          No tienes Cuenta? Registrate aqui
+    <h1 className="text-4xl text-white font-bold text-center">Iniciar Sesión</h1>
+      <nav className="mt-10">
+        <Link to={"/auth/register"} className="text-center text-white text-lg block">
+          ¿No tienes Cuenta? Registrate aqui
         </Link>
       </nav>
     </>
